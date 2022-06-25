43c19a20381e51edcb9029c63ddb75fbcd12c427
sudhanshuprasad <sudhanshuprasad.sp@gmail.com>
2022-06-25 20:30:27 +0530

    adding home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from 'react-bootstrap'
 
 function Home() {
+
+    const [clothItem, setClothItem]=useState();
+
   return (
-    <div>
-        <Card>
-            
-        </Card>
+    <div className='grid-container'>
+      {
+        clothItem?.map((element) => (
+          <Card
+            key={element._id}
+            num={element._id}
+            imgurl="https://picsum.photos/60"
+            foodName={element.name}
+            price={element.price}
+            dsc={element.dsc}
+          />
+        ))
+      }
     </div>
   )
 }
--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -2,43 +2,41 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+function logout(){
+    console.log("logout")
+}
 
 const TopNavBar = () => {
-  return (
-    <div>
-      <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
-            </Nav>
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
-  )
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#">Handlooms.com</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Item>
+                                Home
+                            </Nav.Item>
+                        </Nav>
+                            <NavDropdown className="d-flex mx-5" title="Dropdown" id="nav-dropdown">
+                                <NavDropdown.Item eventKey="4.1">Go</NavDropdown.Item>
+                                <NavDropdown.Item eventKey="4.2">to</NavDropdown.Item>
+                                <NavDropdown.Item eventKey="4.3">hell</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item eventKey="4.4" onClick={logout}>Logout</NavDropdown.Item>
+                            </NavDropdown>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
 }
 
 export default TopNavBar
